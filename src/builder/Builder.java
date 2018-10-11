1401fdc3515512b9d8289513dbb3308ac5ca77f1
beatkei <beatkei@gmail.com>
2018-10-11 21:10:39 +0900

    Builderを追加

---
+++ src/builder/Builder.java
@@ -0,0 +1,8 @@
+package builder;
+
+public abstract class Builder {
+    public abstract void makeTitle(String title);
+    public abstract void makeString(String str);
+    public abstract void makeItem(String[] itemes);
+    public abstract void close();
+}
---
+++ src/builder/Director.java
@@ -0,0 +1,25 @@
+package builder;
+
+public class Director {
+    private Builder builder;
+
+    public Director(Builder builder) {
+        this.builder = builder;
+    }
+    public void construct() {
+        builder.makeTitle("Greeting");
+        builder.makeString("朝から昼にかけて");
+        builder.makeItem(new String[]{
+                "おはようございます",
+                "こんにちは。",
+        });
+        builder.makeString("夜に");
+        builder.makeItem(new String[]{
+                "こんにちは。",
+                "おやすみなし。",
+                "さようなら",
+        });
+        builder.close();
+    }
+
+}
